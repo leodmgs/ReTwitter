5ba35dfc60650450259e1e973c34c68704968fb6
Leonardo Domingues <leodmgs@gmail.com>
2018-12-17 11:21:49 -0300

    Fix scroll bar replacing header cell and set collectioon view background

--- ReTwitter/Controller/HomeDatasourceController.swift
+++ ReTwitter/Controller/HomeDatasourceController.swift
@@ -12,12 +12,16 @@ class HomeDatasourceController: DatasourceController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.backgroundColor = UIColor.init(r: 230, g: 230, b: 230)
         setNavigationBarItems()
         let homeDatasource = HomeDatasource()
         self.datasource = homeDatasource
     }
     
     private func setNavigationBarItems() {
+        if let navBar = navigationController?.navigationBar {
+            navBar.barTintColor = UIColor.white
+        }
         setBarTitleView()
         setLeftBarButtonItem()
         setRightBarButtonItems()
@@ -29,6 +33,13 @@ class HomeDatasourceController: DatasourceController {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 60)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) {
+        // to avoid that a uiview to replace the scroll bar
+        if elementKind == UICollectionView.elementKindSectionHeader {
+            view.layer.zPosition = 0
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
