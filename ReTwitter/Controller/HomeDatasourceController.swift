29e60a282e5605476a4eee2d472eb9922f315779
Leonardo Domingues <leodmgs@gmail.com>
2018-12-18 08:55:39 -0300

    Fix landscape orientation, include some assets and create the Tweet object

--- ReTwitter/Controller/HomeDatasourceController.swift
+++ ReTwitter/Controller/HomeDatasourceController.swift
@@ -10,6 +10,10 @@ import LBTAComponents
 
 class HomeDatasourceController: DatasourceController {
     
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionViewLayout.invalidateLayout()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = UIColor.init(r: 230, g: 230, b: 230)
@@ -36,6 +40,9 @@ class HomeDatasourceController: DatasourceController {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if section == 1 {
+            return CGSize(width: view.frame.width, height: 0)
+        }
         return CGSize(width: view.frame.width, height: 75)
     }
     
---
+++ ReTwitter/Model/Tweet.swift
@@ -0,0 +1,9 @@
+//
+//  Tweet.swift
+//  ReTwitter
+//
+//  Created by Leonardo Domingues on 12/17/18.
+//  Copyright © 2018 Leonardo Domingues. All rights reserved.
+//
+
+import Foundation
